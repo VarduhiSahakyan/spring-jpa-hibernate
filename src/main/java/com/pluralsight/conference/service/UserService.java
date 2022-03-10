22a52fa2fa3924c2614adadeec596e407c427699
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-03-10 17:50:19 +0400

    user interface whit JpaRepository extends deleted UserRepositoryImpl

--- src/main/java/com/pluralsight/conference/service/UserService.java
+++ src/main/java/com/pluralsight/conference/service/UserService.java
@@ -1,11 +1,7 @@
 package com.pluralsight.conference.service;
 
 import com.pluralsight.conference.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-
-@Repository
-public interface UserService extends JpaRepository<User, Long> {
-
+public interface UserService {
+    User save(User user);
 }
